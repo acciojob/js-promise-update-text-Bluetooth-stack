a591b90ad59d1663d10d8a3cb279157d15212919
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-22 20:12:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const output = document.querySelector('#output');
+
+let promise = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world");
+	}, 500)
+});
+
+setTimeout(()=>{
+	promise.then(()=>{
+		output.textContent = "Hello, world!";
+	})
+}, 500)
